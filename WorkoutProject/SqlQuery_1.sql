03fdb591dbb8956af1d3d26704100aa1747ba6a6
Jahnavi <jahnavi.dogiparthi@dxc.com>
2019-05-17 17:43:39 +0530

    DataBase created

--- WorkoutProject/SqlQuery_1.sql
+++ WorkoutProject/SqlQuery_1.sql
@@ -1,16 +1,16 @@
-﻿create database Project
-use Project
+﻿create database Projects
+use Projects
 
 drop table Workout_Category
 --this is comment
-create table WorkOut_Category
+create table WorkOut_Categorys
 (
 category_id int,
 category_name varchar(64),
 constraint pk_category_id primary key(category_id)
 )
 
-create table WorkOut_Collection
+create table WorkOut_Collections
 (
 workout_title varchar(128),
 workout_note varchar(256),
@@ -18,12 +18,13 @@ calories_burn_per_min float,
 category_id int,
 workout_id int,
 constraint pk_workout_id primary key(workout_id),
-constraint fk_WorkOut_Collection_WorkOut_Category foreign key(Category_id) references WorkOut_Category(category_id)
+constraint fk_WorkOut_Collections_WorkOut_Categorys foreign key(Category_id) references WorkOut_Categorys(category_id)
 )
 
 
-create table WorkOut_Active
+create table WorkOut_Actives
 (
+id int identity primary key,
 workout_id int,
 start_time time,
 start_date date,
@@ -31,11 +32,11 @@ end_date date,
 end_time time,
 comment varchar(64),
 status bit,
-constraint fk_WorkOut_Active_WorkOut_Collection foreign key(workout_id)references WorkOut_Collection(workout_id)
+constraint fk_WorkOut_Actives_WorkOut_Collections foreign key(workout_id)references WorkOut_Collections(workout_id)
 )
 
-select * from WorkOut_Category
-select * from WorkOut_Collection
-select * from WorkOut_Active
+select * from WorkOut_Categorys
+select * from WorkOut_Collections
+select * from WorkOut_Actives
 
 
